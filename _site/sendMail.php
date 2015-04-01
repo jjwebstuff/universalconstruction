26118f86aa10b1c276238e3a74f3bc8ccc0b92e0
Jake Siddall <jacobsiddall@gmail.com>
2015-03-31 22:38:53 -0500

    changed to hammersmith

---
+++ _site/sendMail.php
@@ -0,0 +1,23 @@
+<?php 	
+	$name = trim($_POST['name']);
+	
+	$email = trim($_POST['email']);
+			
+	if(function_exists('stripslashes')) {
+		$message = stripslashes(trim($_POST['message']));
+	} else {
+		$message = trim($_POST['message']);
+	}
+
+
+	// Edit below to your needs
+	$emailTo = 'your@email-address.com';
+	$subject = 'Message from '.$name;
+	$sendCopy = trim($_POST['sendCopy']);
+	$body = "Name: $name \n\nEmail: $email \n\nMessage: $message";
+	$headers = 'From: Your Company Website <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+	
+	mail($emailTo, $subject, $body, $headers);
+	
+	return true;
+?>
